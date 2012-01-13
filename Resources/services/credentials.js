46785098d087c6c6c563eee189efd8cb7accd392
Aaron Saunders <aaron@clearlyinnovative.com>
2012-01-13 17:23:59 -0500

    added comment about keys

--- Resources/services/credentials.js
+++ Resources/services/credentials.js
@@ -1,3 +1,7 @@
+//
+// You will need to get your own keys from StakMob, a developer account is free
+// http://www.stackmob.com
+//
 exports.C = {
     STACKMOB_PUBLIC_KEY : '5aca7a3', //replace this with your public
     STACKMOB_PRIVATE_KEY : 'da8cc1bc99', //replace this with your private key
